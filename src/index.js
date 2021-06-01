e7734417075dfb4febb569cfbd4f1056e8e6803d
Rxthew <richard.theuma@gmail.com>
2021-06-01 17:21:08 +0200

    'Update'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase'
+import {firebase, FieldValue} from './lib/firebase'
 
 ReactDOM.render(
+  <FirebaseContext.Provider value={{firebase, FieldValue}}></FirebaseContext.Provider>
   <App />, document.getElementById('root'),
 );
---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//import seed file//
+import { seedDatabase } from '../seed'
+
+const config = {
+    apiKey: "AIzaSyAxIPJtt6V-qrFwVkgIBflyv-ybDmsuLNA",
+    authDomain: "instaclone-cb991.firebaseapp.com",
+    projectId: "instaclone-cb991",
+    storageBucket: "instaclone-cb991.appspot.com",
+    messagingSenderId: "378878539203",
+    appId: "1:378878539203:web:fdd0cea53a37cd87a5b976"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+//call seedfile here (just the one time)//
+seedDatabase (firebase);
+
+export {firebase, FieldValue};
+
