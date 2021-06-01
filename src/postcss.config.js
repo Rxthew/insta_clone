1ee4092c078a63d03ea534273dbe220ce07f565a
Rxthew <richard.theuma@gmail.com>
2021-06-01 23:38:58 +0200

    tailwind css test

---
+++ src/postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable linebreak-style */
+module.exports = {
+  plugins: [require('tailwindcss'), require('autoprefixer')],
+};
