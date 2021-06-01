b70a346966df9590e02324547558c7345276344e
Rxthew <richard.theuma@gmail.com>
2021-06-01 20:22:10 +0200

    Update

---
+++ src/pages/login.js
@@ -0,0 +1,18 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from '../context/firebase';
+
+
+export default function Login(){
+    const history = useHistory();
+    const { firebase } = useContext{FirebaseContext};
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    useEffect(() => {
+        document.title = 'Login - Instaface';
+    } []);
+    return <p>Login page</p>
+}
