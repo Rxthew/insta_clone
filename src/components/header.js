5312c30392fa55562077244e0e11305f5ed72890
Rxthew <richard.theuma@gmail.com>
2021-06-03 01:02:09 +0200

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,12 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom'
+import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
+import * as ROUTES from '../constants/routes' 
+
 export default function Header(){
+    const { firebase } = useContext(FirebaseContext);
+    const { user } = useContext(UserContext);
+
     return <p>head</p>;
    }  
